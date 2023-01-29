57d9276ccbd4021f595a064693e58f95f05dc1b1
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-01-29 14:52:49 -0300

    first commit

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,82 @@
+﻿
+using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Services;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+
+        private FilmeService _filmeService;
+        public FilmeController(FilmeService filmeService) 
+        {
+            _filmeService = filmeService;
+        }
+
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+        {
+            ReadFilmeDto readDto = _filmeService.AdicionaFilme(filmeDto);
+            // Vai ser um readFilmeDto pq esse tipo tem o Id, e eu vou precisar do Id na linha de baixo para retornar o objeto criado.
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = readDto.Id }, readDto);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes([FromQuery] int? classificacaoEtaria = null)
+        {
+            List<ReadFilmeDto> readDto = _filmeService.RecuperaFilmes(classificacaoEtaria);
+            if (readDto != null)
+            { 
+                return Ok(readDto);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            ReadFilmeDto readDto = _filmeService.RecuperaFilmesPorId(id);
+            if (readDto != null)
+            {
+                return Ok(readDto);
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        {
+            Result resultado = _filmeService.AtualizaFilme(id, filmeDto);
+            if (resultado.IsFailed)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            Result resultado = _filmeService.DeletaFilme(id);
+            if (resultado.IsFailed)
+            { 
+                return NoContent();
+            }
+            return NoContent();
+        }
+
+    }
+}
